aa3d36083f1c5599e6190d984d57c372edcf53a9
Max Hooton <max.hooton@auden.co.uk>
2019-05-30 16:48:49 +0100

    see if this deploys

--- .travis-deploy/deploy.sh
+++ .travis-deploy/deploy.sh
@@ -6,6 +6,6 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
        pip install --user awscli
        export PATH=$PATH:$HOME/.local/bin
        aws cloudformation package --template-file template.yaml --s3-bucket=$S3_BUCKET --output-template-file outputtemplate.yml
-       aws cloudformation deploy --template-file outputtemplate.yml --stack-name $STACK_NAME
+       aws cloudformation deploy --template-file outputtemplate.yml --stack-name $STACK_NAME --region $AWS_REGION
     fi
 fi
