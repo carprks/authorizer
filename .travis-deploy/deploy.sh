3850636e696b6c68d711efe218f694c700a3c177
Max Hooton <max.hooton@auden.co.uk>
2019-05-30 16:42:32 +0100

    updated pip

--- .travis-deploy/deploy.sh
+++ .travis-deploy/deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     if [ "$TRAVIS_BRANCH" == "master" ]; then
+       pip install --upgrade python
+       pip install --upgrade pip
        pip install --user awscli
        export PATH=$PATH:$HOME/.local/bin
        aws cloudformation package --template-file template.yaml --s3-bucket=$S3_BUCKET --output-template-file outputtemplate.yml
