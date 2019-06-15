73927d39842b13ff81a142665510e4348eda4762
Keloran <keloran@nordicarts.net>
2019-06-15 14:42:35 +0100

    see if this works

--- .travis-deploy/deploy.sh
+++ .travis-deploy/deploy.sh
@@ -7,11 +7,13 @@ if [[ -z "$TRAVIS_PULL_REQUEST" ]] || [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; t
     AWS_SECRET_ACCESS_KEY=$DEV_AWS_SECRET_ACCESS_KEY
     S3_FOLDER=$DEV_S3_BUCKET
 
-    echo "Key: $AWS_ACCESS_KEY_ID"
-    echo "Sec: $AWS_SECRET_ACCESS_KEY"
-
 #    if [[ -z ${BUCKET} ]] || [[ ${BUCKET} == "" ]]; then
 #        aws s3api
+
+    mkdir .aws
+    echo "[default]" >> .aws/credentials
+    echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> .aws/credentials
+    echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> .aws/credentials
 
     aws s3 cp cf.yaml s3://${S3_FOLDER}/cf.yaml
     aws s3 cp authorizer.zip s3://${S3_FOLDER}/authorizer/authorizer.zip
@@ -21,6 +23,12 @@ if [[ -z "$TRAVIS_PULL_REQUEST" ]] || [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; t
         AWS_ACCESS_KEY_ID=$LIVE_AWS_ACCESS_KEY_ID
         AWS_SECRET_ACCESS_KEY=$LIVE_AWS_SECRET_ACCESS_KEY
         S3_FOLDER=$LIVE_S3_BUCKET
+
+        rm -rf .aws
+        mkdir .aws
+        echo "[default]" >> .aws/credentials
+        echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> .aws/credentials
+        echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> .aws/credentials
 
         aws s3 cp cf.yaml s3://${S3_FOLDER}/cf.yaml
         aws s3 cp authorizer.zip s3://${S3_FOLDER}/authorizer/authorizer.zip
