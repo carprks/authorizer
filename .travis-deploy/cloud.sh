f08f79bc7718cef2b9b504cdd6d758077869b639
Keloran <keloran@nordicarts.net>
2019-06-15 23:21:45 +0100

    folder needs to be in all places

--- .travis-deploy/cloud.sh
+++ .travis-deploy/cloud.sh
@@ -5,16 +5,16 @@ cloudFormation()
 {
     STACK_EXISTS=$(AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE UPDATE_COMPLETE UPDATE_ROLLBACK_COMPLETE --region "$AWS_REGION" | jq '.StackSummaries[].StackName//empty' | grep "$STACK_NAME")
     if [[ -z "$STACK_EXISTS" ]] || [[ "$STACK_EXISTS" == "" ]]; then
-        AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws cloudformation create-stack --template-url https://"$S3_FOLDER".s3."$AWS_REGION".amazonaws.com/cf.yaml --stack-name "$STACK_NAME" --region "$AWS_REGION" --parameters ParameterKey=ServiceName,ParameterValue="$SERVICE_NAME" ParameterKey=BuildKey,ParameterValue="$SERVICE_NAME"/"$TRAVIS_BUILD_ID".zip ParameterKey=Environment,ParameterValue="$DEPLOY_ENV"  ParameterKey=BuildBucket,ParameterValue="$S3_FOLDER" --capabilities CAPABILITY_NAMED_IAM
+        AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws cloudformation create-stack --template-url https://"$S3_FOLDER".s3."$AWS_REGION".amazonaws.com/"$SERVICE_NAME"/cf.yaml --stack-name "$STACK_NAME" --region "$AWS_REGION" --parameters ParameterKey=ServiceName,ParameterValue=$SERVICE_NAME ParameterKey=BuildKey,ParameterValue=$SERVICE_NAME/"$TRAVIS_BUILD_ID".zip ParameterKey=Environment,ParameterValue="$DEPLOY_ENV"  ParameterKey=BuildBucket,ParameterValue="$S3_FOLDER" --capabilities CAPABILITY_NAMED_IAM
     else
-        AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws cloudformation update-stack --template-url https://"$S3_FOLDER".s3."$AWS_REGION".amazonaws.com/cf.yaml --stack-name "$STACK_NAME" --region "$AWS_REGION" --parameters ParameterKey=ServiceName,ParameterValue="$SERVICE_NAME" ParameterKey=BuildKey,ParameterValue="$SERVICE_NAME"/"$TRAVIS_BUILD_ID".zip ParameterKey=Environment,ParameterValue="$DEPLOY_ENV"  ParameterKey=BuildBucket,ParameterValue="$S3_FOLDER" --capabilities CAPABILITY_NAMED_IAM
+        AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws cloudformation update-stack --template-url https://"$S3_FOLDER".s3."$AWS_REGION".amazonaws.com/"$SERVICE_NAME"/cf.yaml --stack-name "$STACK_NAME" --region "$AWS_REGION" --parameters ParameterKey=ServiceName,ParameterValue=$SERVICE_NAME ParameterKey=BuildKey,ParameterValue=$SERVICE_NAME/"$TRAVIS_BUILD_ID".zip ParameterKey=Environment,ParameterValue="$DEPLOY_ENV"  ParameterKey=BuildBucket,ParameterValue="$S3_FOLDER" --capabilities CAPABILITY_NAMED_IAM
     fi
 }
 
 deployIt()
 {
-    AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws s3 cp cf.yaml s3://$S3_FOLDER/cf.yaml
-    AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws s3 cp "$TRAVIS_BUILD_ID".zip s3://$S3_FOLDER/"$SERVICE_NAME"/"$TRAVIS_BUILD_ID".zip
+    AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws s3 cp cf.yaml s3://"$S3_FOLDER"/$SERVICE_NAME/cf.yaml
+    AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws s3 cp "$TRAVIS_BUILD_ID".zip s3://$S3_FOLDER/$SERVICE_NAME/"$TRAVIS_BUILD_ID".zip
 }
 
 if [[ -z "$TRAVIS_PULL_REQUEST" ]] || [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
