6a948930430aff9c46d5e1c5c4127b77663f5191
Keloran <keloran@nordicarts.net>
2019-06-15 14:28:48 +0100

    derp

--- .travis-deploy/cloud.sh
+++ .travis-deploy/cloud.sh
@@ -6,7 +6,7 @@ DEPLOY_ENV=dev
 if [[ -z "$TRAVIS_PULL_REQUEST" ]] || [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     AWS_ACCESS_KEY_ID=$DEV_AWS_ACCESS_KEY_ID
     AWS_SECRET_ACCESS_KEY=$DEV_SECRET_ACCESS_KEY
-    S3_FOLDER=$DEV_S3_FOLDER
+    S3_FOLDER=$DEV_S3_BUCKET
 
     if [[ -z ${authExists} ]] || [[ ${authExists} == "" ]]; then
         aws cloudformation create-stack --template-url s3://${S3_FOLDER}/cf.yaml --stack-name $STACK_NAME --region $AWS_REGION --parameters ParameterKey=ServiceName,ParameterValue=authorizer ParameterKey=BuildKey,ParameterValue=authorizer/authorizer.zip ParameterKey=Environment,ParameterValue=${DEPLOY_ENV}  ParameterKey=BuildBucket,ParameterValue=${S3_FOLDER} --capabilities CAPABILITY_NAMED_IAM
@@ -18,7 +18,7 @@ if [[ -z "$TRAVIS_PULL_REQUEST" ]] || [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; t
     if [[ "$TRAVIS_BRANCH" == "master" ]]; then
         AWS_ACCESS_KEY_ID=$LIVE_AWS_ACCESS_KEY_ID
         AWS_SECRET_ACCESS_KEY=$LIVE_SECRET_ACCESS_KEY
-        S3_FOLDER=$LIVE_S3_FOLDER
+        S3_FOLDER=$LIVE_S3_BUCKET
         DEPLOY_ENV=live
 
         if [[ -z ${authExists} ]] || [[ ${authExists} == "" ]]; then
--- .travis-deploy/deploy.sh
+++ .travis-deploy/deploy.sh
@@ -5,20 +5,20 @@ BUCKET=$(aws s3api list-buckets | jq '.Buckets[].Name//empty' | grep "${S3_FOLDE
 if [[ -z "$TRAVIS_PULL_REQUEST" ]] || [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     AWS_ACCESS_KEY_ID=$DEV_AWS_ACCESS_KEY_ID
     AWS_SECRET_ACCESS_KEY=$DEV_SECRET_ACCESS_KEY
-    S3_FOLDER=$DEV_S3_FOLDER
+    S3_FOLDER=$DEV_S3_BUCKET
 
 #    if [[ -z ${BUCKET} ]] || [[ ${BUCKET} == "" ]]; then
 #        aws s3api
 
-    aws s3 cp cf.yaml s3://$S3_FOLDER/cf.yaml
-    aws s3 cp authorizer.zip s3://$S3_FOLDER/authorizer/authorizer.zip
+    aws s3 cp cf.yaml s3://${S3_FOLDER}/cf.yaml
+    aws s3 cp authorizer.zip s3://${S3_FOLDER}/authorizer/authorizer.zip
 
     if [[ "$TRAVIS_BRANCH" == "master" ]]; then
         AWS_ACCESS_KEY_ID=$LIVE_AWS_ACCESS_KEY_ID
         AWS_SECRET_ACCESS_KEY=$LIVE_SECRET_ACCESS_KEY
-        S3_FOLDER=$LIVE_S3_FOLDER
+        S3_FOLDER=$LIVE_S3_BUCKET
 
-        aws s3 cp cf.yaml s3://$S3_FOLDER/cf.yaml
-        aws s3 cp authorizer.zip s3://$S3_FOLDER/authorizer/authorizer.zip
+        aws s3 cp cf.yaml s3://${S3_FOLDER}/cf.yaml
+        aws s3 cp authorizer.zip s3://${S3_FOLDER}/authorizer/authorizer.zip
     fi
 fi
