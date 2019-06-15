030588ad745d4489e9eb1ff090db4b59e0a3ad9d
Keloran <keloran@nordicarts.net>
2019-06-15 14:33:05 +0100

    derp

--- .travis-deploy/cloud.sh
+++ .travis-deploy/cloud.sh
@@ -5,7 +5,7 @@ DEPLOY_ENV=dev
 
 if [[ -z "$TRAVIS_PULL_REQUEST" ]] || [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     AWS_ACCESS_KEY_ID=$DEV_AWS_ACCESS_KEY_ID
-    AWS_SECRET_ACCESS_KEY=$DEV_SECRET_ACCESS_KEY
+    AWS_SECRET_ACCESS_KEY=$DEV_AWS_SECRET_ACCESS_KEY
     S3_FOLDER=$DEV_S3_BUCKET
 
     if [[ -z ${authExists} ]] || [[ ${authExists} == "" ]]; then
@@ -14,10 +14,10 @@ if [[ -z "$TRAVIS_PULL_REQUEST" ]] || [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; t
         aws cloudformation update-stack --template-url s3://${S3_FOLDER}/cf.yaml --stack-name $STACK_NAME --region $AWS_REGION --parameters ParameterKey=ServiceName,ParameterValue=authorizer ParameterKey=BuildKey,ParameterValue=authorizer/authorizer.zip ParameterKey=Environment,ParameterValue=${DEPLOY_ENV}  ParameterKey=BuildBucket,ParameterValue=${S3_FOLDER}
     fi
 
-# Master has an extra deployment
+# Master has an extra step to launch into live
     if [[ "$TRAVIS_BRANCH" == "master" ]]; then
         AWS_ACCESS_KEY_ID=$LIVE_AWS_ACCESS_KEY_ID
-        AWS_SECRET_ACCESS_KEY=$LIVE_SECRET_ACCESS_KEY
+        AWS_SECRET_ACCESS_KEY=$LIVE_AWS_SECRET_ACCESS_KEY
         S3_FOLDER=$LIVE_S3_BUCKET
         DEPLOY_ENV=live
 
--- .travis-deploy/deploy.sh
+++ .travis-deploy/deploy.sh
@@ -4,7 +4,7 @@ BUCKET=$(aws s3api list-buckets | jq '.Buckets[].Name//empty' | grep "${S3_FOLDE
 
 if [[ -z "$TRAVIS_PULL_REQUEST" ]] || [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     AWS_ACCESS_KEY_ID=$DEV_AWS_ACCESS_KEY_ID
-    AWS_SECRET_ACCESS_KEY=$DEV_SECRET_ACCESS_KEY
+    AWS_SECRET_ACCESS_KEY=$DEV_AWS_SECRET_ACCESS_KEY
     S3_FOLDER=$DEV_S3_BUCKET
 
 #    if [[ -z ${BUCKET} ]] || [[ ${BUCKET} == "" ]]; then
@@ -13,9 +13,10 @@ if [[ -z "$TRAVIS_PULL_REQUEST" ]] || [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; t
     aws s3 cp cf.yaml s3://${S3_FOLDER}/cf.yaml
     aws s3 cp authorizer.zip s3://${S3_FOLDER}/authorizer/authorizer.zip
 
+# Master has an extra step to launch into live
     if [[ "$TRAVIS_BRANCH" == "master" ]]; then
         AWS_ACCESS_KEY_ID=$LIVE_AWS_ACCESS_KEY_ID
-        AWS_SECRET_ACCESS_KEY=$LIVE_SECRET_ACCESS_KEY
+        AWS_SECRET_ACCESS_KEY=$LIVE_AWS_SECRET_ACCESS_KEY
         S3_FOLDER=$LIVE_S3_BUCKET
 
         aws s3 cp cf.yaml s3://${S3_FOLDER}/cf.yaml
